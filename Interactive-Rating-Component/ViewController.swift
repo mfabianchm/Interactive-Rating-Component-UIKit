9450bf1b34434fea912e3b42095a66fdacf4d177
mfabianchm <mfchongm@hotmail.com>
2024-04-14 16:44:15 -0600

    Initial Commit

---
+++ Interactive-Rating-Component/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interactive-Rating-Component
+//
+//  Created by Marcos Fabian Chong Megchun on 14/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
